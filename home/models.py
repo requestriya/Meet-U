7b404ec6e82dfb934cf2671ceb0d70aec20f11be
SinghIsCoding <theriyasingh3@gmail.com>
2021-09-07 02:52:11 +0530

    .

--- home/models.py
+++ home/models.py
@@ -6,13 +6,14 @@ from django.contrib.auth.models import User
 class User_Profile (models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     dp = models.ImageField(upload_to='dp', blank=True, null=True)
-    images = models.ImageField(upload_to='images', blank=True, null=True)
+    images = models.ImageField(upload_to='dp', blank=True, null=True)
     bio = models.CharField(max_length=200)
     interest = models.CharField(max_length=200)
     age = models.DateField()
     gender = models.CharField(max_length=50)
+    
 
 class User_images(models.Model):
     proimage = models.ForeignKey(User_Profile, default=None, on_delete=models.CASCADE)
-    images2 = models.ImageField(null=True, blank=True)
+    images2 = models.ImageField(upload_to='dp', null=True, blank=True)
 
--- meetyou/settings.py
+++ meetyou/settings.py
@@ -119,5 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_URL = '/dp/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'dp/')
+
